2c5c736fbb83087b9ee110258ca9c7629e2c237d
Szőcs Áron <szocs.aa@gmail.com>
2023-12-14 15:22:21 +0100

    init contianer app

---
+++ ContainerApp/ContainerApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using ContainerApp.Zoo;
+using System;
+
+namespace ContainerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var opener = new ZooOpener();
+            opener.Open();
+
+            Console.WriteLine("END");
+        }
+    }
+}
